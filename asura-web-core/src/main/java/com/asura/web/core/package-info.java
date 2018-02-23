79b126e259c0915e81c98841b70b969e4c3e740f
liusq23 <liusq23@ziroom.com>
2018-02-23 15:16:52 +0800

    init

---
+++ asura-web-core/src/main/java/com/asura/web/core/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @FileName: package-info.java
+ * @Package: com.asura.web.core
+ * @author liusq23
+ * @created 2018/2/23 下午3:15
+ * <p>
+ * Copyright 2018 asura
+ */
+package com.asura.web.core;
---
+++ asura-web-sample/src/main/java/com/asura/web/sample/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @FileName: package-info.java
+ * @Package: com.asura.web.sample
+ * @author liusq23
+ * @created 2018/2/23 下午3:16
+ * <p>
+ * Copyright 2018 asura
+ */
+package com.asura.web.sample;
