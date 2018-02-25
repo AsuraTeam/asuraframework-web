bb4f0279dbf57ec37093399904475e77c8a3dfb7
liusq23 <liusq23@ziroom.com>
2018-02-26 00:31:40 +0800

    rename package

--- asura-web-autoconfig/src/main/java/com/asura/web/autoconfig/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * @FileName: package-info.java
- * @Package: com.asura.web.autoconfig
- * @author liusq23
- * @created 2018/2/23 下午3:15
- * <p>
- * Copyright 2018 asura
- */
-package com.asura.web.autoconfig;
--- asura-web-starter/src/main/java/org/asuraframework/web/starter/package-info.java
+++ asura-web-starter/src/main/java/org/asuraframework/web/starter/package-info.java
@@ -1,9 +1,9 @@
 /**
  * @FileName: package-info.java
- * @Package: com.asura.web.sample
+ * @Package: org.asuraframework.web.starter
  * @author liusq23
  * @created 2018/2/23 下午3:16
  * <p>
  * Copyright 2018 asura
  */
-package com.asura.web.sample;
+package org.asuraframework.web.starter;
